8692df8a3340074dd9d75fe77a01c70d14821c55
fznsakib <fznsakib@gmail.com>
2020-02-22 18:41:50 +0000

    Fix number of channels of network layers

--- spatial.py
+++ spatial.py
@@ -3,54 +3,60 @@ import torch.nn as nn
 import glob
 
 
-class Temporal_CNN(nn.Module):
-    def __init__(self, height: int, width: int, channels: int, : dropout: float):
+class CNN(nn.Module):
+    def __init__(self, height: int, width: int, channels: int, dropout: float):
         super().__init__()
         self.input_shape = ImageShape(height=224, width=224, channels=10)
         self.dropout = nn.Dropout(dropout)
 
-        self.conv1 = nn.Conv3d(
+        self.conv1 = nn.Conv2d(
             in_channels=self.input_shape.channels,
-            out_channels=256,
-            kernel_size=(7, 7, 96),
-            stride=2
+            out_channels=96,
+            kernel_size=(7, 7),
+            stride=2,
+            bias=False
         )
-        self.batchNorm1 = nn.BatchNorm3d(256)
+        self.localResponseNorm1 = nn.LocalResponseNorm(5, alpha=0.001, beta=0.75, k=2.0)
         self.pool1 = nn.MaxPool2d(kernel_size=(3, 3), stride=(2, 2), ceil_mode=True)
         
-        self.conv2 = nn.Conv3d(
-            in_channels=256,
-            out_channels=512,
-            kernel_size=(5, 5, 256),
-            stride=2
+        self.conv2 = nn.Conv2d(
+            in_channels=96,
+            out_channels=256,
+            kernel_size=(5, 5),
+            stride=2,
+            bias=False
         )
-        self.batchNorm2 = nn.BatchNorm3d(512)
+        self.localResponseNorm2 = nn.LocalResponseNorm(5, alpha=0.001, beta=0.75, k=2.0)
         self.pool2 = nn.MaxPool2d(kernel_size=(3, 3), stride=(2, 2), ceil_mode=True)
 
         self.conv3 = nn.Conv3d(
-            in_channels=512,
+            in_channels=256,
             out_channels=512,
-            kernel_size=(3, 3, 512),
-            stride=1
+            kernel_size=(3, 3),
+            stride=1,
+            bias=False
         )
         
         self.conv4 = nn.Conv3d(
             in_channels=512,
             out_channels=512,
-            kernel_size=(3, 3, 512),
-            stride=1
+            kernel_size=(3, 3),
+            stride=1,
+            bias=False
         )
         
         self.conv5 = nn.Conv3d(
             in_channels=512,
             out_channels=512,
-            kernel_size=(3, 3, 512),
-            stride=1
+            kernel_size=(3, 3),
+            stride=1,
+            bias=False
         )
         self.pool5 = nn.MaxPool2d(kernel_size=(3, 3), stride=(2, 2), ceil_mode=True)
         
         # TODO: Figure out the correct number of inputs for fc1
-        self.fc1 = nn.Linear(20000, 4096)
+        # Input is the size of the output from conv5 multiplied by the 512 channels
+        self.fc1 = nn.Linear(20000, 4096, bias=False)
         self.initialise_layer(self.fc1)
         
         self.fc2 = nn.Linear(1024, 9, bias=False)
@@ -60,14 +66,14 @@ class Temporal_CNN(nn.Module):
     def forward(self, images: torch.Tensor) -> torch.Tensor:
         # First convolutional layer pass
         x = self.conv1(images)
-        x = self.batchNorm1(x)
         x = F.relu(x)
+        x = self.localResponseNorm1(x)
         x = self.pool(x)
 
         # Second convolutional layer pass
         x = self.conv2(x)
-        x = self.batchNorm2(x)
         x = F.relu(x)
+        x = self.localResponseNorm2(x)
         x = self.pool2(x)
 
         # Third convolutional layer pass
@@ -81,7 +87,7 @@ class Temporal_CNN(nn.Module):
         # # Flatten the output of the pooling layer so it is of shape
         # # (32, 15488), ready for fc1 to take in as input.
         # x = torch.flatten(x, start_dim=1, end_dim=3)
-        x = self.conv(5)
+        x = self.conv5(x)
         x = F.relu(x)
         
         # First fully connected layer pass
