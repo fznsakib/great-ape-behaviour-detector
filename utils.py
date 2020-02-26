f0dca37efbfd9482fc4144e6bb8731aa6cde1e50
fznsakib <fznsakib@gmail.com>
2020-02-26 17:11:05 +0000

    Resolve tensor cpu/gpu issues

--- utils.py
+++ utils.py
@@ -9,14 +9,15 @@ class ImageShape(NamedTuple):
     
 # Compute the fusion logits across the spatial and temporal stream to perform average fusion
 def average_fusion(spatial_logits, temporal_logits):
-    spatial_logits = spatial_logits.detach().numpy()
-    temporal_logits = temporal_logits.detach().numpy()
+    spatial_logits = spatial_logits.cpu().detach().numpy()
+    temporal_logits = temporal_logits.cpu().detach().numpy()
     fusion_logits = np.mean(np.array([spatial_logits, temporal_logits]), axis=0)
 
     return fusion_logits
 
 # Compute the top1 accuracy of predictions made by the network
 def compute_accuracy(labels, predictions):
+    labels = labels.cpu()
     predictions = torch.from_numpy(predictions)
     assert len(labels) == len(predictions)
     return float((labels == predictions).sum()) / len(labels)
