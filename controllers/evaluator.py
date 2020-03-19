c0cdd420b88402a6fcc53c7d9f22b6b63a3da66a
fznsakib <fznsakib@gmail.com>
2020-03-19 10:12:49 +0000

    Printing accuracy stats in testing

--- controllers/evaluator.py
+++ controllers/evaluator.py
@@ -31,6 +31,7 @@ class Evaluator:
 
         # Turn on evaluation for networkss
         self.cnn.model.eval()
+        results = {"labels": [], "logits": [], "predictions": []}
 
         #  No need to track gradients for validation, we're not optimizing.
         with torch.no_grad():
@@ -50,6 +51,11 @@ class Evaluator:
 
                 # Accumulate predictions against ground truth labels
                 prediction = logits.argmax().item()
+                
+                # Collect reulsts for metrics
+                results["labels"].append(label.item())
+                results["logits"].append(logits.tolist())
+                results["predictions"].append(prediction)
 
                 # Insert results to dictionary
                 if video not in self.predictions.keys():
@@ -63,5 +69,53 @@ class Evaluator:
                         "start_frame": start_frame.item(),
                     }
                 )
+                
+        # Get accuracy by checking for correct predictions across all predictions
+        top1, top3 = metrics.compute_topk_accuracy(
+            torch.LongTensor(results["logits"]), torch.LongTensor(results["labels"]), topk=(1, 3)
+        )
+
+        # Get per class accuracies and sort by label value (0...9)
+        class_accuracy = metrics.compute_class_accuracy(results["labels"], results["predictions"])
+        class_accuracy_average = mean(class_accuracy.values())
+        
+        print("==> Per Class Results")
+        per_class_results = [
+            [
+                "Class",
+                "camera_interaction",
+                "climbing_down",
+                "climbing_up",
+                "hanging",
+                "running",
+                "sitting",
+                "sitting_on_back",
+                "standing",
+                "walking"
+            ],
+            [
+                "Accuracy",
+                f'{class_accuracy[0]:2.2f}',
+                f'{class_accuracy[1]:2.2f}',
+                f'{class_accuracy[2]:2.2f}',
+                f'{class_accuracy[3]:2.2f}',
+                f'{class_accuracy[4]:2.2f}',
+                f'{class_accuracy[5]:2.2f}',
+                f'{class_accuracy[6]:2.2f}',
+                f'{class_accuracy[7]:2.2f}',
+                f'{class_accuracy[8]:2.2f}'
+            ]
+        ]
+        
+        print(tabulate(per_class_results, tablefmt="fancy_grid"))
+        
+        print("==> Overall Results")
+        test_results = [
+            ["Average Class Accuracy:", f"{class_accuracy_average:2f}"],
+            ["Top1 Accuracy:", f"{top1.item():.2f}"],
+            ["Top3 Accuracy:", f"{top3.item():.2f}"],
+        ]
+
+        print(tabulate(test_results, tablefmt="fancy_grid"))
 
         return self.predictions
